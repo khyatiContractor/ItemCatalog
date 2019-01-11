2d20446246386738d004d5359f9e8f6633f2826f
Khyati Contractor <44378055+khyatiContractor@users.noreply.github.com>
2019-01-11 00:00:46 -0800

    Updating port in project.py

--- vagrant/ItemCatalog/project.py
+++ vagrant/ItemCatalog/project.py
@@ -406,4 +406,4 @@ def showDetails(item_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
